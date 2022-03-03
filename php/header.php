58d5afa553212dcd520b4faa2d1082353e4a9efb
Matteo Berlanas <matteobm89250@gmail.com>
2022-03-03 16:52:00 +0100

    first task hugo

---
+++ php/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
+    <title>Magasin guitare</title>
+</head>
+<body>
+
+<div class="navbar">
+    <a href="index.php">
+    <p>Base de donnée guitare</p>
+    </a>
+    <form action="add_guitare.php">        
+        <button>Ajouter une guitare</button>
+    </form>
+</div>
